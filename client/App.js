cc9ab254fccab894c804819b35a82129abbbeacd
JoeJLin <Joe.j.lin21@gmail.com>
2019-03-12 18:56:56 -0400

    add navigation

--- client/App.js
+++ client/App.js
@@ -1,43 +1,56 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { StyleSheet, Text, View } from "react-native";
 import axios from "axios";
 import { API_PATH } from "./config/keys";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state ={
-      name: ""
-    }
-  }
-  componentWillMount() {
-    axios
-      .get(`${API_PATH}/api`)
-      .then(response => {
-        this.setState({ name: response.data.name });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>{this.state.name}</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const MainNavigator = createStackNavigator({
+  Home: { screen: HomeScreen },
+  Profile: { screen: ProfileScreen }
 });
 
+const App = createAppContainer(MainNavigator);
+
 export default App;
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state ={
+//       name: ""
+//     }
+//   }
+//   componentWillMount() {
+//     axios
+//       .get(`${API_PATH}/api`)
+//       .then(response => {
+//         this.setState({ name: response.data.name });
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Open up App.js to start working on your app!</Text>
+//         <Text>{this.state.name}</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+// export default App;
