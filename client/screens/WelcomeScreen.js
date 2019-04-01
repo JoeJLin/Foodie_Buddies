59660c5d716d185256b902b0f877c4b4c053f253
JoeJLin <Joe.j.lin21@gmail.com>
2019-03-31 23:01:24 -0400

    add user auth

--- client/screens/WelcomeScreen.js
+++ client/screens/WelcomeScreen.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
-import { Button, View, Text } from "react-native";
+import {
+  Button,
+  View,
+  Text,
+  AsyncStorage,
+  ActivityIndicator,
+  StatusBar
+} from "react-native";
 
 class WelcomeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.checkUserToken();
+  }
+
+  checkUserToken = async () => {
+    const userToken = await AsyncStorage.getItem("UserToken");
+
+    this.props.navigation.navigate(userToken ? "Home" : "Auth");
+  };
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Button
-          title="Login"
-          onPress={() => this.props.navigation.navigate("Home")}
-        />
-        <Button title="Sign Up" onPress={() => alert("welcome")} />
+      <View>
+        <ActivityIndicator />
+        <StatusBar barStyle="default" />
       </View>
     );
   }
