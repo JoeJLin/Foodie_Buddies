01cd8ed11e0a57e2526c18921bf58fe8f4016906
JoeJLin <Joe.j.lin21@gmail.com>
2019-04-05 17:28:53 -0400

    pull master

--- client/screens/CalenderScreen.js
+++ client/screens/CalenderScreen.js
@@ -1,36 +1,30 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import CalendarPicker from 'react-native-calendar-picker';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import CalendarPicker from "react-native-calendar-picker";
 
 class CalenderScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedStartDate: null,
+      selectedStartDate: null
     };
     this.onDateChange = this.onDateChange.bind(this);
   }
 
   onDateChange(date) {
     this.setState({
-      selectedStartDate: date,
+      selectedStartDate: date
     });
   }
   render() {
     const { selectedStartDate } = this.state;
-    const startDate = selectedStartDate ? selectedStartDate.toString() : '';
+    const startDate = selectedStartDate ? selectedStartDate.toString() : "";
     return (
       <View style={styles.container}>
-        <CalendarPicker
-          onDateChange={this.onDateChange}
-        />
+        <CalendarPicker onDateChange={this.onDateChange} />
 
         <View>
-          <Text>SELECTED DATE:{ startDate }</Text>
+          <Text>SELECTED DATE:{startDate}</Text>
         </View>
       </View>
     );
@@ -40,9 +34,9 @@ class CalenderScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
-    marginTop: 100,
-  },
+    backgroundColor: "#FFFFFF",
+    marginTop: 100
+  }
 });
 
 export default CalenderScreen;
