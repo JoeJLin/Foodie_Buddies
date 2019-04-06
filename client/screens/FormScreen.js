fe0a6985f4a6640468eb76c74f669b4745b0ad3f
JoeJLin <Joe.j.lin21@gmail.com>
2019-04-06 01:50:59 -0400

    add a roomcode to private room

--- client/screens/FormScreen.js
+++ client/screens/FormScreen.js
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
-import { DatePickerIOS,ScrollView, View,StyleSheet,Image,Modal,TouchableHighlight, TouchableOpacity} from "react-native";
+import {
+  DatePickerIOS,
+  ScrollView,
+  View,
+  StyleSheet,
+  Image,
+  Modal,
+  TouchableHighlight,
+  TouchableOpacity
+} from "react-native";
 import {
   Container,
   Header,
@@ -13,195 +22,196 @@ import {
   Left,
   Right,
   Body,
-  Form
+  Form,
+  Input,
+  Item
 } from "native-base";
-import RNPickerSelect from 'react-native-picker-select';
-import DateTimePicker from 'react-native-modal-datetime-picker';
+import RNPickerSelect from "react-native-picker-select";
+import DateTimePicker from "react-native-modal-datetime-picker";
 import CalenderScreen from "./CalenderScreen";
 
 const People = [
   {
-    label: '1 person',
-    value: '1 person',
+    label: "1 person",
+    value: "1 person"
   },
   {
-    label: '2 people',
-    value: '2 people',
+    label: "2 people",
+    value: "2 people"
   },
   {
-    label: '3 people',
-    value: '3 people',
+    label: "3 people",
+    value: "3 people"
   },
   {
-    label: '4 people',
-    value: '4 people',
+    label: "4 people",
+    value: "4 people"
   },
   {
-    label: '5 people',
-    value: '5 people',
+    label: "5 people",
+    value: "5 people"
   },
   {
-    label: '6 people',
-    value: '6 people',
+    label: "6 people",
+    value: "6 people"
   },
   {
-    label: '7 people',
-    value: '7 people',
+    label: "7 people",
+    value: "7 people"
   },
   {
-    label: '8 people',
-    value: '8 people',
+    label: "8 people",
+    value: "8 people"
   },
   {
-    label: '9 people',
-    value: '9 people',
+    label: "9 people",
+    value: "9 people"
   },
   {
-    label: '10 people',
-    value: '10 people',
-  },
+    label: "10 people",
+    value: "10 people"
+  }
 ];
-const Roomtype =[{
-  label: 'Public',
-  value: 'Public'
-},
-  {label: 'Private',
-  value: 'Private'
-}
-]
+const Roomtype = [
+  {
+    label: "Public",
+    value: "Public"
+  },
+  { label: "Private", value: "Private" }
+];
 class FormScreen extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      RoomValue : 'Select Roomsize',
-      PrivateOrPublic: 'Select Room Type',
+    this.state = {
+      RoomValue: "Select Roomsize",
+      PrivateOrPublic: "Select Room Type",
       isDateTimePickerVisible: false,
       isTimePickerVisible: false,
-      dateValue: 'Select Date',
-      TimeValue: 'Select Time',
-      isPrivate:false,
-    }
-  };
+      dateValue: "Select Date",
+      TimeValue: "Select Time",
+      isPrivate: false,
+      roomCode: ""
+    };
+  }
 
   _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
 
   _hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
 
-  _handleDatePicked = (date) => {
+  _handleDatePicked = date => {
     date = new Date().toDateString();
 
-    console.log('A date has been picked: ', date);
+    console.log("A date has been picked: ", date);
     this._hideDateTimePicker();
-    this.setState({dateValue:date})
+    this.setState({ dateValue: date });
   };
 
   _showTimePicker = () => this.setState({ isTimePickerVisible: true });
 
   _hideTimePicker = () => this.setState({ isTimePickerVisible: false });
 
-  _handleTimePicked = (date) => {
+  _handleTimePicked = date => {
     date = new Date().toTimeString();
 
-    console.log('A date has been picked: ', date);
+    console.log("A date has been picked: ", date);
     this._hideTimePicker();
-    this.setState({TimeValue:date})
+    this.setState({ TimeValue: date });
   };
- 
-// Determin the Room type by resetting the isPrivate state
 
-  DeterminRoomType = (PriviateOrPublic) =>{
-    console.log('Here')
-    console.log(this.state.isPrivate)
+  // Determin the Room type by resetting the isPrivate state
 
-      if(PriviateOrPublic==='Private')
-      {
-        this.setState({ isPrivate: true });
+  DeterminRoomType = () => {
+    console.log("Here");
+    console.log(this.state.isPrivate);
 
-        console.log('Hello');
-        console.log(this.state.isPrivate);
-      }
-      else{
-        // this.setState({
-        //   isPrivate:false
-        // })
-        console.log('Kugou');
-        console.log(this.state.isPrivate);
-      }
-      
-  }
+    if (this.state.PrivateOrPublic === "Private") {
+      this.setState({ isPrivate: true });
+
+      console.log("in private");
+      console.log(this.state.isPrivate);
+    } else {
+      this.setState({
+        isPrivate: false
+      });
+      console.log("in public");
+    }
+  };
 
   render() {
-    const {navigate} = this.props.navigation; //Defind for Navagation
+    const { navigate } = this.props.navigation; //Defind for Navagation
     const { navigation } = this.props;
     //const time = navigation.getParam('time', 'Select Time');
     const placeholder = {
-      label: 'Select',
+      label: "Select",
       value: null,
-      color: '#151719',
+      color: "#151719"
     };
     const typleHolder = {
-      label: 'Select',
+      label: "Select",
       value: null,
-      color: '#151719',
+      color: "#151719"
     };
-
+    const roomCode = (
+      <CardItem>
+        {/* <Content> */}
+        <Item regular>
+          <Input placeholder="Enter room code" />
+        </Item>
+        {/* </Content> */}
+      </CardItem>
+    );
     return (
       <Container style={styles.container}>
-        <Image style={{
-            width : 100+'%',
+        <Image
+          style={{
+            width: 100 + "%",
             height: 150
-          }} 
+          }}
           source={{
-            uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS22ybkAttkcFp4y9vUNupY-RyE-iME6Qr8emVpaLHs-X4UzkeI"
-          }}/>
+            uri:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS22ybkAttkcFp4y9vUNupY-RyE-iME6Qr8emVpaLHs-X4UzkeI"
+          }}
+        />
 
-       <Content padder>
+        <Content padder>
           <Card style={styles.mb}>
             <CardItem header bordered>
-              <Text>Make a Reservaton</Text>
+              <Text>Create room</Text>
             </CardItem>
 
             <CardItem>
               <Left>
-                <Icon
-                  active
-                  name="ios-people"
-                  style={{ color: "black" }}
-                /> 
-               <ScrollView>
-                <RNPickerSelect
-                  placeholder={typleHolder}
-                  items={People}
-                  onValueChange={value => {
-                    this.setState({
-                      RoomValue: value,
-                    });
-                  }}
-                  style={pickerSelectStyles}
+                <Icon active name="ios-people" style={{ color: "black" }} />
+                <ScrollView>
+                  <RNPickerSelect
+                    placeholder={typleHolder}
+                    items={People}
+                    onValueChange={value => {
+                      this.setState({
+                        RoomValue: value
+                      });
+                    }}
+                    style={pickerSelectStyles}
                   />
                 </ScrollView>
               </Left>
               <Right>
                 <Icon name="arrow-forward" />
               </Right>
-            </CardItem>     
-            
+            </CardItem>
+
             <CardItem>
               <Left>
-                <Icon
-                  active
-                  name="ios-calendar"
-                  style={{ color: "#3B579D" }}
-                />
-                 <TouchableOpacity onPress={this._showDateTimePicker}>
-                <Text>{this.state.dateValue}</Text>
+                <Icon active name="ios-calendar" style={{ color: "#3B579D" }} />
+                <TouchableOpacity onPress={this._showDateTimePicker}>
+                  <Text>{this.state.dateValue}</Text>
                 </TouchableOpacity>
                 <DateTimePicker
-          isVisible={this.state.isDateTimePickerVisible}
-          onConfirm={this._handleDatePicked}
-          onCancel={this._hideDateTimePicker}
-          mode={'date'}
-        />
+                  isVisible={this.state.isDateTimePickerVisible}
+                  onConfirm={this._handleDatePicked}
+                  onCancel={this._hideDateTimePicker}
+                  mode={"date"}
+                />
               </Left>
               <Right>
                 <Icon name="arrow-forward" />
@@ -210,20 +220,16 @@ class FormScreen extends Component {
 
             <CardItem>
               <Left>
-                <Icon
-                  active
-                  name="ios-time"
-                  style={{ color: "#3B579D" }}
-                />
-                 <TouchableOpacity onPress={this._showTimePicker}>
-                <Text>{this.state.TimeValue}</Text>
+                <Icon active name="ios-time" style={{ color: "#3B579D" }} />
+                <TouchableOpacity onPress={this._showTimePicker}>
+                  <Text>{this.state.TimeValue}</Text>
                 </TouchableOpacity>
                 <DateTimePicker
-          isVisible={this.state.isTimePickerVisible}
-          onConfirm={this._handleTimePicked}
-          onCancel={this._hideTimePicker}
-          mode={'time'}
-        />
+                  isVisible={this.state.isTimePickerVisible}
+                  onConfirm={this._handleTimePicked}
+                  onCancel={this._hideTimePicker}
+                  mode={"time"}
+                />
               </Left>
               <Right>
                 <Icon name="arrow-forward" />
@@ -238,52 +244,34 @@ class FormScreen extends Component {
                 <Icon name="arrow-forward" />
               </Right>
             </CardItem>
-           
+
             <CardItem>
               <Left>
-                <Icon
-                  active
-                  name="ios-lock"
-                  style={{ color: "black" }}
-                /> 
-               <ScrollView>
-                <RNPickerSelect
-                  placeholder={typleHolder}
-                  items={Roomtype}
-                  onValueChange={value => {
-                    this.setState({
-                      PrivateOrPublic: value,
-                    });
-                    
-                    
-                  }}
-
-                  // Calling the OnDone function and call determinRoom Type function inside
-                  onDonePress={()=>{
-                    //console.log(this.state.PrivateOrPublic);
-                    this.DeterminRoomType(this.state.PrivateOrPublic);
-                      if(this.state.isPrivate){
-                         return <CalenderScreen/>
-                      }               
-                  }}
-                  style={pickerSelectStyles}
+                <Icon active name="ios-lock" style={{ color: "black" }} />
+                <ScrollView>
+                  <RNPickerSelect
+                    placeholder={typleHolder}
+                    items={Roomtype}
+                    onValueChange={value => {
+                      this.setState({
+                        PrivateOrPublic: value
+                      });
+                    }}
+                    onDonePress={() => this.DeterminRoomType()}
+                    style={pickerSelectStyles}
                   />
                 </ScrollView>
               </Left>
               <Right>
                 <Icon name="arrow-forward" />
               </Right>
-                  
-            </CardItem> 
-
+            </CardItem>
+            {this.state.isPrivate ? roomCode : null}
           </Card>
-              <Button info style={{alignSelf:'center'}}>
-                <Text>
-                  Sumbit
-                </Text>
-            </Button>
+          <Button info style={{ alignSelf: "center" }}>
+            <Text>Sumbit</Text>
+          </Button>
         </Content>
-       
       </Container>
     );
   }
@@ -291,9 +279,9 @@ class FormScreen extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     backgroundColor: "#FFF",
-    justifyContent: "center", 
+    justifyContent: "center"
     //alignItems: "center",
   },
   mb: {
@@ -302,13 +290,13 @@ const styles = StyleSheet.create({
 });
 const pickerSelectStyles = StyleSheet.create({
   inputIOS: {
-    left:5,
+    left: 5,
     fontSize: 16,
     paddingVertical: 12,
     paddingHorizontal: 10,
-    color: 'black',
-    paddingRight: 30, // to ensure the text is never behind the icon
-  },
+    color: "black",
+    paddingRight: 30 // to ensure the text is never behind the icon
+  }
 });
 
 export default FormScreen;
