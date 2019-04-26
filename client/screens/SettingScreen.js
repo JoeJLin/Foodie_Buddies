3692f44891fc54819b07dfa4c7359aa15ef97c9b
weibin  xian <xianweibin@email.com>
2019-04-25 22:13:21 -0400

    error

--- client/screens/SettingScreen.js
+++ client/screens/SettingScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Button, View, Text, AsyncStorage } from "react-native";
-
+import ImagePicker from "react-native-image-picker";
 class Setting extends React.Component {
   constructor(props) {
     super(props);
@@ -8,7 +8,7 @@ class Setting extends React.Component {
       userId: ""
     };
   }
-
+ 
   async componentDidMount() {
     const userId = await AsyncStorage.getItem("userId");
     this.setState({ userId });
@@ -21,12 +21,31 @@ class Setting extends React.Component {
     console.log("aosnfoinaoisnfoiasndonafsnoini")
     this.props.navigation.navigate("Auth");
   };
+  handleChoosePhoto=()=>{
+    const options = {
+      title: 'Select Avatar',
+      customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchImageLibrary(options, (response) => {
+      // Same code as in above section!
+
+    });
+  }
 
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>{this.state.userId}</Text>
         <Button onPress={() => this.signOut()} title="Sign Out" />
+        <Button
+          title="Choose Photo"
+          onPress={ this.handleChoosePhoto}
+        />
+        
       </View>
     );
   }
