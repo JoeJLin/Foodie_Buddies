e3455849144ebafff9ee3564d4e1436a2d17d9f2
JoeJLin <Joe.j.lin21@gmail.com>
2019-04-30 19:15:36 -0400

    fix bugs

--- utils/room.js
+++ utils/room.js
@@ -8,23 +8,25 @@ const getAllRooms = (latitude, longitude) => {
     console.log(latitude, longitude);
     console.log("in get all room");
     Room.find({
-      // isPast: false,
-      location: {
-        $near: {
-          $maxDistance: 1000000000,
-          // $minDistance: 100,
-          $geometry: {
-            type: "Point",
-            coordinates: [longitude, latitude]
+        // isPast: false,
+        location: {
+          $near: {
+            $maxDistance: 100,
+            // $minDistance: 100,
+            $geometry: {
+              type: "Point",
+              coordinates: [longitude, latitude]
+            }
           }
         }
-      }
-    })
+      })
       // .limit(20)
       .then(results => {
         // console.log(results);
         if (results.length === 0) {
-          reject({ err: "No locations found!" });
+          reject({
+            err: "No locations found!"
+          });
           return;
         }
         getAllRoomsInfo(results)
